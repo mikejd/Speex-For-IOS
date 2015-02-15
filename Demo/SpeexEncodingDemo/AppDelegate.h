bf0bf263d4824fef13b326e97ad4431cd5008bd9
Mikhail Dudarev <mikejd@mihteh.com>
2015-02-15 14:48:17 +0300

    Simple demo project added.

---
+++ Demo/SpeexEncodingDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpeexEncodingDemo
+//
+//  Created by Mikhail Dudarev (mikejd@mikejd.ru) on 09.05.13.
+//  Copyright (c) 2013 Mihteh Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/SpeexEncodingDemo/AppDelegate.m
@@ -0,0 +1,63 @@
+//
+//  AppDelegate.m
+//  SpeexEncodingDemo
+//
+//  Created by Mikhail Dudarev (mikejd@mikejd.ru) on 09.05.13.
+//  Copyright (c) 2013 Mihteh Lab. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "WaveParser.h"
+#import "SpeexEncoder.h"
+#import <Speex/Speex.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    // *** ENCODER TESTING STARTS HERE ***
+    
+    NSString *waveFilePath = [[NSBundle mainBundle] pathForResource:@"test_44100Hz_16bit" ofType:@"wav"];
+    NSError *encodingError;
+    SpeexEncoder *spxEncoder = [SpeexEncoder encoderWithMode:speex_wb_mode quality:6 outputSampleRate:SAMPLE_RATE_16000_HZ];
+    NSData *spx = [spxEncoder encodeWaveFileAtPath:waveFilePath error:&encodingError];
+    NSLog(@"ENCODED DATA: %@", spx);
+    
+    // *** ENCODER TESTING ENDS HERE ***
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
